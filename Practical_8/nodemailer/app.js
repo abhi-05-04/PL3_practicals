827a256801cc0c977ac39745d3d20870c09a7692
Abhishek <abhishk.charpale@walcandsangli.ac.in>
2021-11-24 18:02:30 +0530

    updated nodemodular

--- Practical_8/nodemailer/app.js
+++ Practical_8/nodemailer/app.js
@@ -6,29 +6,29 @@ const nodemail = require('nodemailer');
 const port = 3000;
 const app = express();
 
-// app.get()
-// const sendmail = async (mail) => {
-//     var transport = nodemail.createTransport({
-//         service: 'Gmail',
-//         auth: {
-//             user: 'charpaleabhi@gmail.com',
-//             pass: '*********'
-//         }
-//     });
-//     console.log('Transport created');
-//     transport.sendMail({
-//         from:'charpaleabhi@gmail.com',
-//         to:mail,
-//         subject:'This is module mail',
-//         text: 'This is module mail'
-//     })
-// }
+app.get()
+const sendmail = async (mail) => {
+    var transport = nodemail.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'charpaleabhi@gmail.com',
+            pass: '*********'
+        }
+    });
+    console.log('Transport created');
+    transport.sendMail({
+        from:'charpaleabhi@gmail.com',
+        to:mail,
+        subject:'This is module mail',
+        text: 'This is module mail'
+    })
+}
 
-// app.get('/',(req,res)=>{
-//     const mail = req.query.mail
-//     sendmail(mail)
-//     res.send('Email sent successfully.')
-// })
+app.get('/',(req,res)=>{
+    const mail = req.query.mail
+    sendmail(mail)
+    res.send('Email sent successfully.')
+})
 app.listen(port,()=>{
     console.log('listening on port '+port);
 });
